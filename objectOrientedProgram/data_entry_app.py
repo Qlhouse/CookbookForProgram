e3a2fa0c2f3cf559aa8fa6fa31e9dbb0f6af1644
Qlhouse <31285678+Qlhouse@users.noreply.github.com>
2022-03-19 16:55:25 +0800

    Update data_entry_app.py

--- objectOrientedProgram/data_entry_app.py
+++ objectOrientedProgram/data_entry_app.py
@@ -3,3 +3,9 @@ from pathlib import Path
 import csv
 import tkinter as tk
 from tkinter import ttk
+
+class BoundText(tk.Text):
+    """A Text widget with a bound variable"""
+    def __init__(self, *args, textvariable=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._variable = textvariable
